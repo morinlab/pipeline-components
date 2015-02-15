22f4be83ef9d8e12596668bb6e06741954fd65d2
Bruno Grande <bgrande@sfu.ca>
2015-02-14 21:19:03 -0800

    initial implementation of the run_ensembl_vep component

---
+++ run_ensembl_vep/component_params.py
@@ -0,0 +1,31 @@
+"""
+component_params.py
+
+@author: bgrande
+"""
+
+
+input_files  = {"input_file": "__REQUIRED__"}
+
+output_files = {"output_file": "__REQUIRED__"}
+
+input_params = {"dir": "__REQUIRED__",
+                "fasta": "__REQUIRED__",
+                "assembly": "GRCh37",
+                "fork": "1",
+                "quiet": True,
+                "offline": True,
+                "no_stats": True,
+                "everything": True,
+                "check_existing": True,
+                "total_length": True,
+                "allele_number": True,
+                "no_escape": True,
+                "gencode_basic": True,
+                "xref_refseq": True,
+                "vcf": True}
+
+return_value = []
+
+
+
---
+++ run_ensembl_vep/component_reqs.py
@@ -0,0 +1,20 @@
+"""
+component_reqs.py
+
+@author: bgrande
+"""
+
+env_vars = {}
+
+memory = '1G'
+
+parallel = False
+
+requirements = {"perl": "perl",
+                "variant_effect_predictor": None}
+
+## TODO: here goes the version of the component seed.
+seed_version = 'v76'
+
+## TODO: here goes the version of the component itself.
+version = '1.0.0'
