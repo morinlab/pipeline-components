ec4e805de3d71008bc27baf1dcd2ee55b28f67c6
Jasleen Grewal <jgrewal@xhost09.bcgsc.ca>
2015-02-27 18:33:48 -0800

    Added picard_markdup component

---
+++ picard_markdup/component_params.py
@@ -0,0 +1,30 @@
+"""
+component_params.py
+@author: jgrewal
+"""
+
+## here goes the list of the input files. Use flags: 
+## '__REQUIRED__' to make it required
+## '__FLAG__' to make it a flag or switch.
+input_files  = {
+                 'input_file' : '__REQUIRED__', 
+                }
+
+## TODO: here goes the list of the output files.
+output_files = {
+                 'output_file' : '__REQUIRED__',
+                }
+
+## TODO: here goes the list of the input parameters excluding input/output files.
+input_params = {
+                 'metrics_file' : '__REQUIRED__',
+#                 'input_param2' : '__FLAG__',
+#                 'input_param3' : None
+                }
+
+## TODO: here goes the return value of the component_seed. 
+## DO NOT USE, Not implemented yet!
+return_value = []
+
+                    
+                    
---
+++ picard_markdup/component_ui.py
@@ -0,0 +1,21 @@
+"""
+component_ui.py
+@author: jgrewal
+"""
+
+import argparse
+
+#==============================================================================
+# make a UI 
+#==============================================================================
+## TODO: pass the name of the component to the 'prog' parameter and a
+## brief description of your component to the 'description' parameter.
+parser = argparse.ArgumentParser(prog='picard_markdup', 
+                                 description = "This tool takes in a coordinate sorted BAM file and generates a markdup'd BAM file from it.")
+## TODO: create the list of input options here. Add as many as desired.
+parser.add_argument("--input_file", required=True, help="Coordinate sorted BAM file")
+parser.add_argument("--output_file",required=True,help="Name of output BAM file")
+parser.add_argument("--metrics_file",required=True,help="Name of output Metrics file")                    
+
+## parse the argument parser.
+args, unknown = parser.parse_known_args()
