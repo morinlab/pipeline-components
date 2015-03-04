f01c172a9e6002ca3748ff7649c614300a670963
Jasleen Grewal <jgrewal@xhost09.bcgsc.ca>
2015-03-04 12:39:08 -0800

    Started splitbam component

---
+++ splitbam/component_main.py
@@ -0,0 +1,52 @@
+""" 
+component_main.py
+This module contains Component class which extends 
+the ComponentAbstract class. It is the core of a component.
+
+Note the places you need to change to make it work for you. 
+They are marked with keyword 'TODO'.
+"""
+
+from pipeline_factory.utils import ComponentAbstract
+import os
+
+
+class Component(ComponentAbstract):
+    
+    """
+    TODO: add component doc here. 
+    """
+
+    def __init__(self, component_name="splitbam", 
+                 component_parent_dir=None, seed_dir=None):
+        
+        ## TODO: pass the version of the component here.
+        self.version = "v0.99.0"
+
+        ## initialize ComponentAbstract
+        super(Component, self).__init__(component_name, 
+                                        component_parent_dir, seed_dir)
+
+    ## TODO: write the focus method if the component is parallelizable.
+    ## Note that it should return cmd, cmd_args.
+    def focus(self, cmd, cmd_args, chunk):
+        pass 
+#         return cmd, cmd_args
+
+    ## TODO: this method should make the command and command arguments 
+    ## used to run the component_seed via the command line. Note that 
+    ## it should return cmd, cmd_args. 
+    def make_cmd(self, chunk=None):
+        pass
+#         return cmd, cmd_args
+
+## To run as stand alone
+def _main():
+    m = Component()
+    m.args = component_ui.args
+    m.run()
+
+if __name__ == '__main__':
+    import component_ui
+    _main()
+
