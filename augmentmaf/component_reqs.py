a68d5663068677740c515a329623433dcc044fa6
jasgrewal <grewalj23@gmail.com>
2015-03-16 15:04:26 -0700

    Added component for augmenting vcf2maf maf outputs

---
+++ augmentmaf/component_reqs.py
@@ -0,0 +1,28 @@
+"""
+component_reqs.py
+@author: jgrewal
+"""
+
+## here goes the list of the environment variables, if any,
+## required to export for the component to function properly.
+env_vars = {
+# 			'env_var2' : 'value3'
+			}
+
+## here goes the max amount of the memory required. 
+memory = '5G'
+
+## set this to True if the component is parallelizable.
+parallel = False
+
+## here goes the list of the required software/apps
+## called by the component.  
+requirements = {
+ 				'python': '/path/python/bin/python',
+				}
+
+## here goes the version of the component seed.
+seed_version = '1.00.0'
+
+## here goes the version of the component itself. 
+version = '1.00.0'
