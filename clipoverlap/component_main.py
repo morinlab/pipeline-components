f3a79957abfde1ba80a9f46c46f6be3140a2420e
Bruno Grande <contact@brunogrande.me>
2015-03-19 11:55:46 -0700

    fixed bamUtils requirement

--- clipoverlap/component_main.py
+++ clipoverlap/component_main.py
@@ -1,6 +1,6 @@
-""" 
+"""
 component_main.py
-This module contains Component class which extends 
+This module contains Component class which extends
 the ComponentAbstract class. It is the core of a component.
 
 @author: jgrewal
@@ -12,7 +12,7 @@ from pipeline_factory.utils import ComponentAbstract
 import os
 
 class Component(ComponentAbstract):
-    
+
     """
 	This BamUtils tool generates a softclipped BAM from an input BAM file.
 	Input:
@@ -20,29 +20,29 @@ class Component(ComponentAbstract):
 		- Name of Output file
     """
 
-    def __init__(self, component_name="clipoverlap", 
+    def __init__(self, component_name="clipoverlap",
                  component_parent_dir=None, seed_dir=None):
-        
+
         ## pass the version of the component here.
         self.version = "v2.16"
 
         ## initialize ComponentAbstract
-        super(Component, self).__init__(component_name, 
+        super(Component, self).__init__(component_name,
                                         component_parent_dir, seed_dir)
 
     ## write the focus method if the component is parallelizable.
     ## Note that it should return cmd, cmd_args.
     def focus(self, cmd, cmd_args, chunk):
-        pass 
+        pass
 #         return cmd, cmd_args
 
-    ## this method should make the command and command arguments 
-    ## used to run the component_seed via the command line. Note that 
-    ## it should return cmd, cmd_args. 
+    ## this method should make the command and command arguments
+    ## used to run the component_seed via the command line. Note that
+    ## it should return cmd, cmd_args.
     def make_cmd(self, chunk=None):
-	path=os.path.join(self.requirements['bamutils'],'bam')
+	path = self.requirements['bamutils_bam_bin']
 	cmd = path + ' clipOverlap '
-	cmd_args= ['--in '+self.args.input_file, '--out '+self.args.output_file]
+	cmd_args = ['--in '+self.args.input_file, '--out '+self.args.output_file]
 	return cmd, cmd_args
 
 ## To run as stand alone
--- clipoverlap/component_reqs.py
+++ clipoverlap/component_reqs.py
@@ -10,21 +10,18 @@ env_vars = {
 # 			'env_var2' : 'value3'
 			}
 
-## here goes the max amount of the memory required. 
+## here goes the max amount of the memory required.
 memory = '5G'
 
 ## set this to True if the component is parallelizable.
 parallel = False
 
 ## here goes the list of the required software/apps
-## called by the component.  
-requirements = {
-		'bamutils': '/path/to/bamutils/bam',
-#		'python': '/path/python/bin/python',
-				}
+## called by the component.
+requirements = {'bamutils_bam_bin': '/path/to/bamutils/bin/bam'}
 
 ## here goes the version of the component seed.
 seed_version = '2.16'
 
-## here goes the version of the component itself. 
+## here goes the version of the component itself.
 version = '1.0.0'
