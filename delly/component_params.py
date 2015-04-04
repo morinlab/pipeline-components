f0729225a8a7ccdb2c8ec68c204da141eadeb5f8
Bruno Grande <contact@brunogrande.me>
2015-04-04 12:32:29 -0700

    first implementation of DELLY component

---
+++ delly/component_params.py
@@ -0,0 +1,21 @@
+"""
+component_params.py
+
+@author: bgrande
+"""
+
+
+input_files = {'bam_files': '__REQUIRED__'}
+
+output_files = {'vcf_file': '__REQUIRED__'}
+
+input_params = {'sv_type': '__REQUIRED__',
+                'reference_fasta': '__REQUIRED__',
+                'excluded_regions': '__OPTIONAL__',
+                'map_qual': '__OPTIONAL__',
+                'mad_cutoff': '__OPTIONAL__',
+                'min_flank': '__OPTIONAL__',
+                'vcfgeno': '__OPTIONAL__',
+                'geno_qual': '__OPTIONAL__'}
+
+return_value = []
