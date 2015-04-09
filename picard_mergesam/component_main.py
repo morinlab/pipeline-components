b580cd826379f184b0af7bd0b1edda6e0917c172
jasgrewal <grewalj23@gmail.com>
2015-04-08 19:13:04 -0700

    silly debugging

--- picard_mergesam/component_main.py
+++ picard_mergesam/component_main.py
@@ -51,7 +51,7 @@ class Component(ComponentAbstract):
 	cmd_args =  ['INPUT='+infiles, 'OUTPUT='+self.args.output_file,'SORT_ORDER='+self.args.sort_order,'USE_THREADING='+self.args.use_threading]
 	if(self.args.delete_input=="true"):
 		infiles_rm = ' rm '.join(glob.glob(self.args.input_dir+"/"+self.args.input_regex))
-		cmd_args = cmd_args + ["; rm "+infiles_rm] +
+		cmd_args = cmd_args + ["; rm "+infiles_rm]
 	return cmd, cmd_args
 
     def test(self):
