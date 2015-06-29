0bfa4a36a6718df2b943d6d142f779230b17343f
jasgrewal <jasgrewal@users.noreply.github.com>
2015-06-29 14:05:29 -0700

    Update component_params.py
    Updated java mem option

--- picard_mergesam/component_params.py
+++ picard_mergesam/component_params.py
@@ -17,6 +17,7 @@ output_files = {
 
 ## here goes the list of the input parameters excluding input/output files.
 input_params = {
+		'java_mem' : '4G',
                 'val_stringency' : 'SILENT', #'__REQUIRED__',
                 'verbosity' : 'INFO',
                 'quiet' : 'false',
