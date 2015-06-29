9e65c69a25634595b6ba3381e68e778414a09898
jasgrewal <jasgrewal@users.noreply.github.com>
2015-06-29 14:07:36 -0700

    Updated GC overhead option for java instance

--- picard_markdup/component_main.py
+++ picard_markdup/component_main.py
@@ -43,7 +43,7 @@ class Component(ComponentAbstract):
     ## it should return cmd, cmd_args. 
     def make_cmd(self, chunk=None):
 	path=os.path.join(self.requirements['picardtools'], 'MarkDuplicates.jar')
-	cmd = self.requirements['java'] + 'java -Xmx4G' + ' -jar ' + path
+	cmd = self.requirements['java'] + 'java -Xmx' + self.args.java_mem + ' -XX:-UseGCOverheadLimit -XX:-UseParallelGC ' + ' -jar ' + path
 	cmd_args = ['INPUT='+self.args.input_file,'VERBOSITY='+self.args.verbosity,
 			'QUIET='+self.args.quiet,
 			'VALIDATION_STRINGENCY='+self.args.val_stringency, 'OUTPUT='+self.args.output_file, 'METRICS_FILE='+self.args.metrics_file]
