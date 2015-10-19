12d1b2b757b80cd811dfa85f0b6e25cfca0c7281
Bruno Grande <bgrande@sfu.ca>
2015-10-19 16:35:07 -0700

    updated component_main and removed tests

--- bwa_mem_parallelized/component_main.py
+++ bwa_mem_parallelized/component_main.py
@@ -6,7 +6,6 @@ component_main.py
 
 import glob
 import os.path
-import re
 import logging
 from pipeline_factory.utils import ComponentAbstract
 import component_test
@@ -30,8 +29,24 @@ class Component(ComponentAbstract):
         if len(files) > 1:
             logging.warn("Found more than one FASTQ file that matches the chunk pattern: {}".format(pattern))
         elif len(files) == 0:
-            logging.error("No FASTQ files matches the chunk pattern: {}".format(pattern))
-        return args_dict
+            raise ValueError("No FASTQ files matches the chunk pattern: {}".format(pattern))
+        fastq_file = files[0]
+        args_dict["reads_fastq"] = fastq_file
+        # Determine if paired or unpaired, and update BWA options accordingly
+        if "unpaired" in fastq_file:
+            args_dict["p"] = False
+        elif "paired" in fastq_file:
+            args_dict["p"] = True
+        # Set output file
+        exts = ["fastq.gz", "fq.gz", "fastq", "fq"]
+        base = os.path.basename(fastq_file)
+        for ext in exts:
+            if fastq_file.endswith(ext):
+                base = fastq_file[:-len(ext)]
+                break
+        args_dict["output_bam"] = os.path.join(args_dict["output_dir"], base + ".bam")
+        # Return
+        return
 
     def make_cmd(self, chunk=None):
 
@@ -52,13 +67,13 @@ class Component(ComponentAbstract):
             self.focus(args_dict, chunk)
 
         # Extract special arguments
-        spec_args = ["output_dir"]
+        spec_args = ["input_dir", "output_dir"]
         spec_args_dict = {k: v for k, v in args_dict.items() if k in spec_args}
         for arg in spec_args:
             del args_dict[arg]
 
         # Extract positional arguments
-        pos_args = ["bwa_index_prefix", "reads_fastq", "mates_fastq"]  # Order matters here
+        pos_args = ["bwa_index_prefix", "reads_fastq"]  # Order matters here
         pos_args_dict = {k: v for k, v in args_dict.items() if k in pos_args}
         for arg in pos_args:
             del args_dict[arg]
@@ -92,10 +107,10 @@ class Component(ComponentAbstract):
                 logging.warn("Command-line argument skipped: {}".format(arg))
 
         # Add positional arguments
-        cmd_args.extend([pos_args_dict[arg] for arg in pos_args])
+        cmd_args.extend([pos_args_dict[arg] for arg in pos_args if arg in pos_args_dict])
 
         # Handle special arguments
-        cmd_args.extend(["|", "samtools", "view", "-b", "-S", "-", ">", spec_args_dict["output"]])  # Convert SAM to BAM
+        cmd_args.extend(["|", "samtools", "view", "-b", "-S", "-", ">", spec_args_dict["output_bam"]])  # Convert SAM to BAM
 
         # Return cmd and cmg_args
         return cmd, cmd_args
