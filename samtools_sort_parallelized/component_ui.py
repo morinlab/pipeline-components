b5157a5cbe40d4e5aeabaa96a6478eebdd6d64cf
Prasath <ppararaj@gmail.com>
2015-10-30 00:29:38 -0700

    Initial commit

---
+++ samtools_sort_parallelized/component_ui.py
@@ -0,0 +1,15 @@
+"""
+component_ui.py
+
+@author: ppararaj
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser(prog='samtools_sort_parallelized',
+                                 description="""Sort BAM file.""")
+
+parser.add_argument("input_bam", help="Unsorted BAM file.")
+parser.add_argument("output_bam", help="Sorted BAM file.")
+
+args, unknown = parser.parse_known_args()
