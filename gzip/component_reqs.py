5c225e1130718933e1d3798e0716461dbe0f0309
Prasath <ppararaj@gmail.com>
2015-11-01 01:34:53 -0800

    Initial commit.

---
+++ gzip/component_reqs.py
@@ -0,0 +1,17 @@
+"""
+component_reqs.py
+
+@author: ppararaj
+"""
+
+env_vars = {}
+
+memory = '6G'
+
+parallel = False
+
+requirements = {"gzip":"gzip"}
+
+seed_version = 'v1.6'
+
+version = 'v1.0.0'
