3b64038a6bb1d2fb11272f0a41d411b510c9f06d
Prasath <ppararaj@gmail.com>
2015-10-31 20:08:48 -0700

    Initial commit.

---
+++ merge_mpileup/component_reqs.py
@@ -0,0 +1,18 @@
+"""
+component_reqs.py
+
+@author: ppararaj
+"""
+
+env_vars = {}
+
+memory = '6G'
+
+parallel = False
+
+requirements = {"python": "python"
+	           "merge_mpileup": "merge_mpileup.py"}
+
+seed_version = 'v1.0'
+
+version = 'v1.0.0'
