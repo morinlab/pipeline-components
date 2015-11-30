ad928d6eb29bfb069de054ffefefa51989c1e698
Bruno Grande <bgrande@sfu.ca>
2015-11-29 17:52:02 -0800

    added sequenza_analysis

---
+++ sequenza_analysis/component_params.py
@@ -0,0 +1,21 @@
+"""
+component_params.py
+
+@author: bgrande
+"""
+
+
+input_files = {"input": "__OPTIONAL__"}
+
+output_files = {"output": "__OPTIONAL__"}
+
+input_params = {"chr": "__OPTIONAL__",
+                "sample_id": "__OPTIONAL__",
+                "gender": "__OPTIONAL__",
+                "min_reads": "__OPTIONAL__",
+                "min_normal_reads": "__OPTIONAL__",
+                "cellularity_limit": "__OPTIONAL__",
+                "ploidy_limit": "__OPTIONAL__",
+                "max_cn": "__OPTIONAL__"}
+
+return_value = []
