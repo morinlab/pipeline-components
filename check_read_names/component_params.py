af46cf8b6ace27429e0be7c148237134b64d49d3
ppararaj <ppararaj@gmail.com>
2015-12-22 19:19:24 -0800

    Initial commit

---
+++ check_read_names/component_params.py
@@ -0,0 +1,19 @@
+"""
+component_params.py
+
+@author: ppararaj
+"""
+
+
+input_files = {"read_set": "__OPTIONAL__",
+               "original_bam": "__OPTIONAL__",
+               "new_fastq_filename": "__OPTIONAL__",
+               "new_bam_filename": "__OPTIONAL__"}
+
+output_files = {"read_set_out": "__OPTIONAL__"}
+
+parameters = {"files_to_delete": "__OPTIONAL__",
+              "files_to_delete_path": "__OPTIONAL__",
+              "new_file_path": "__OPTIONAL__"}
+
+return_value = []
