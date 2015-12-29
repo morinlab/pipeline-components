e4633cb6d7a1eea8c6d9aee87bb78aa7bbe1dc71
ppararaj <ppararaj@gmail.com>
2015-12-29 05:09:32 -0800

    Initial commit.

---
+++ pysam_bam2fq/component_reqs.py
@@ -0,0 +1,18 @@
+"""
+component_reqs.py
+
+@author: ppararaj
+"""
+
+env_vars = {}
+
+memory = '6G'
+
+parallel = False
+
+requirements = {"python": "python",
+                "pysam_bam2fq": "pysam_bam2fq"}
+
+seed_version = 'v1.0.0'
+
+version = 'v1.0.0'
---
+++ pysam_bam2fq/component_ui.py
@@ -0,0 +1,17 @@
+"""
+component_ui.py
+
+@author: ppararaj
+"""
+
+import argparse
+
+
+parser = argparse.ArgumentParser(prog='bam2fq',
+                                 description="""Converts BAM to FASTQ.""")
+parser.add_argument('bam', help='BAM to convert to FASTQ.')
+parser.add_argument('output_dir', default='./',
+                    help='Output directory of the FASTQ files.')
+parser.add_argument('--num_reads', '-n', type=int, default=75000000,
+                    help='Maximum number of reads per FASTQ file.')
+args, unknown = parser.parse_known_args()
