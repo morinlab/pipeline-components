3624390d0f8885ce286e07a70d0f70c94c286736
ppararaj <ppararaj@gmail.com>
2016-01-08 17:19:00 -0800

    Updated components to take directory and files parameters.

--- hash_read_names/component_params.py
+++ hash_read_names/component_params.py
@@ -7,8 +7,10 @@ component_params.py
 
 input_files = {"original_bam": "__OPTIONAL__",
                "hash_sum_infile": "__OPTIONAL__",
-               "new_fastqs": "__OPTIONAL__",
-               "new_bams": "__OPTIONAL__"}
+               "directory": "__OPTIONAL__",
+               "files": "__OPTIONAL__",
+               "new_fastqs": "__FLAG__",
+               "new_bams": "__FLAG__"}
 
 output_files = {"hash_sum_outfile": "__OPTIONAL__"}
 
--- hash_read_names/component_ui.py
+++ hash_read_names/component_ui.py
@@ -8,17 +8,21 @@ import argparse
 
 parser = argparse.ArgumentParser(prog='hash_read_names',
                                  description="""Uses hash funciton to check for read name integrity.""")
+
 parser.add_argument('--original_bam',
                     help='Path to original BAM.')
 parser.add_argument('--hash_sum_outfile', type=argparse.FileType('w'),
-                    help='Output file for the hash sum of original BAM reads.')
+                    help='Output file for the hash sum.')
 parser.add_argument('--hash_sum_infile', type=argparse.FileType('r'),
                     help='File containing a hash sum that will be used to check \
                     integrity of either new fastq or new bam files.')
-parser.add_argument('--new_fastqs', nargs='*',
-                    help='Path(s) or a single glob to FASTQ files to check for \
-                    read name integrity.')
-parser.add_argument('--new_bams', nargs='*',
-                    help='Path(s) or a single glob to FASTQ files to check for \
-                    read name integrity.')
+parser.add_argument('--directory',
+                    help='Directory containing FASTQ or BAM files to hash.')
+parser.add_argument('--files', nargs='*',
+                    help="FASTQ or BAM files in '--directory' to hash. Can be a glob.")
+parser.add_argument('--new_fastqs', action=store_true,
+                    help="Flag indicating files specified in '--files' are FASTQs.")
+parser.add_argument('--new_bams', action=store_true,
+                    help="Flag indicating files specified in '--files' are BAMs.")
+
 args, unknown = parser.parse_known_args()
