657023f753f8bcb6c006c8f3bff205b6d3606775
Bruno Grande <bgrande@sfu.ca>
2016-02-05 13:19:07 -0800

    Remove legacy components (moved to dev branch)

--- file_eater/component_test/commands.sh
+++
@@ -1,3 +0,0 @@
-factory init_pipeline -c compfile.yaml -p comparer
-python __FACTORY_WORKING_DIR__/comparer.py -c /home/jgrewal/projects/lab_pipeline/pipeline-components/ -w ./ --drmaa_library_path 'lib/lx24-amd64/libdrmaa.so'
-
