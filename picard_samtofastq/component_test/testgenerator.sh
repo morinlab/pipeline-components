988a3d1fd53a5d3554a063bbd61efafc7ebc6038
Bruno Grande <bgrande@sfu.ca>
2016-02-08 09:52:08 -0800

    Move legacy components to archive branch

--- picard_samtofastq/component_test/testgenerator.sh
+++
@@ -1,2 +0,0 @@
-#Temporary notes file. Running from home local
-python /Users/jgrewal/morin-pipeline/pipeline-factory/pipeline_factory/factory.py -w ./ make_config -c picard_samtofastq_pipeline picard_samtofastq
