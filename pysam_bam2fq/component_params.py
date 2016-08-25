957a62989b363b61b6aa7470153b5bdfbabb19bd
ppararaj <ppararaj@gmail.com>
2016-08-25 16:18:34 -0700

    Added trim_bases paramter

--- pysam_bam2fq/component_params.py
+++ pysam_bam2fq/component_params.py
@@ -10,6 +10,7 @@ input_files = {"bam": "__REQUIRED__"}
 output_files = {"output_dir": "__REQUIRED__",
                 "interval_file": "__REQUIRED__"}
 
-input_params = {"num_reads": "__OPTIONAL__"}
+input_params = {"num_reads": "__OPTIONAL__",
+                "trim_bases": "__OPTIONAL__"}
 
 return_value = []
